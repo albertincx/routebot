a3ea142495068618e1a673c096fb4b3bb6f0e2cb
albert <albertincx@gmail.com>
2021-11-10 09:10:54 +0300

    update

---
+++ src/api/routes/route.js
@@ -0,0 +1,81 @@
+const keyboards = require('../../keyboards/keyboards');
+const messages = require('../../messages/format');
+const BUTTONS = require('../../config/buttons');
+const db = require('../utils/db');
+const logger = require('../utils/logger');
+
+const rabbitmq = require('../../service/rabbitmq');
+const BotHelper2 = require('./bot/route');
+
+rabbitmq.startChannel();
+global.lastIvTime = +new Date();
+
+/**
+ * @param ctx
+ * @param botHelper BotHelper
+ */
+const broadcast = (ctx, botHelper) => {
+  const {
+    chat: {id: chatId},
+    text,
+  } = ctx.message;
+  if (!botHelper.isAdmin(chatId) || !text) {
+    return;
+  }
+
+  db.processBroadcast(text, ctx, botHelper);
+};
+
+const format = (bot, botHelper) => {
+  const BH2 = new BotHelper2(botHelper);
+  bot.command(['/createBroadcast', '/startBroadcast'], ctx =>
+    broadcast(ctx, botHelper),
+  );
+  bot.hears(BUTTONS.driver.label, ctx => BH2.driverType(ctx, 1));
+  bot.hears(BUTTONS.sharingDriver.label, ctx => BH2.driverType(ctx, 2));
+  bot.hears(BUTTONS.passenger.label, ctx => BH2.driverType(ctx, 3));
+  bot.command(BUTTONS.driver.command, ctx => BH2.driverType(ctx, 1));
+  bot.command(BUTTONS.sharingDriver.command, ctx => BH2.driverType(ctx, 2));
+  bot.command(BUTTONS.passenger.command, ctx => BH2.driverType(ctx, 3));
+
+  bot.action(/.*/, async ctx => {
+    const [data] = ctx.match;
+    logger('action');
+    console.log(ctx);
+    if (data.match(/start_agree/)) {
+      try {
+        await ctx.reply(messages.start2(), keyboards.start());
+      } catch (e) {
+        // system = `${e}${system}`;
+      }
+      // await bot.telegram
+      //   .editMessageText(from.id, message_id, null, RESULT, keyboards.start())
+      //   .catch(console.log);
+    }
+  });
+
+  bot.on('chosen_inline_result', ({chosenInlineResult}) => {
+    console.log('chosen inline result', chosenInlineResult);
+  });
+  function test(c, t) {
+    console.log(c.update.message.location, t, '2');
+  }
+  bot.hears(/.*/, ctx => test(ctx, 1));
+  bot.on('message', ctx => test(ctx, 2));
+
+  const jobMessage = async task => {
+    //
+    console.log(task);
+  };
+
+  try {
+    setTimeout(() => {
+      rabbitmq.run(jobMessage);
+      rabbitmq.runSecond(jobMessage);
+    }, 5000);
+  } catch (e) {
+    botHelper.sendError(e);
+  }
+};
+
+module.exports = format;
