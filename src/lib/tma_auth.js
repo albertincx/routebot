f1114a8826e90add1b5556c041748c20c472674c
albertincx <albertincx@gmail.com>
2024-07-08 11:56:15 +0700

    auth fix

--- src/lib/tma_auth.js
+++ src/lib/tma_auth.js
@@ -1,6 +1,7 @@
 const {validate} = require('@tma.js/init-data-node');
 
 const TG_ADMIN = parseInt(process.env.TGADMIN, 10);
+const TG_ADMIN_2 = parseInt(process.env.TGADMIN_2, 10);
 
 const validateTmaAuth = (initData) => {
   let result = false;
@@ -24,26 +25,24 @@ const auth = (req, res, next) => {
       }
     }
     let validTgUser = validateTmaAuth(authData);
-
+    if (process.env.DEV) {
+      validTgUser = true;
+    }
     if (validTgUser) {
       const url = new URL(`https://test/?${authData}`).searchParams.get('user');
       const parsedUser = JSON.parse(url);
+      let isValidId = parsedUser && [TG_ADMIN, TG_ADMIN_2].includes(parsedUser.id);
 
-      if (parsedUser.id !== TG_ADMIN) {
-        //
-      } else {
+      if (isValidId) {
         req.user = parsedUser;
+        return next();
       }
     }
 
-    if (req.user) {
-      next();
-    } else {
-      res.status(401)
-        .json({
-          error: new Error('Invalid request!'),
-        });
-    }
+    res.status(401)
+      .json({
+        error: new Error('Invalid request!'),
+      });
   } catch (e) {
     next(e);
   }
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,26 +6,21 @@ const User = mongoose.model('User');
 const db = require('../api/utils/db');
 
 // Validate an existing user and issue a JWT
-router.post('/login', (req, res, next) => {
+router.post('/settings', async (req, res, next) => {
   let u = req.user;
-  User.findOne({username: u.username})
-    .then(async user => {
-      if (!user) {
-        await db.updateUser(u);
-        user = u;
-      }
-      if (!user) {
-        res.status(401)
-          .json({success: false, msg: 'could not find user'});
-        return;
-      }
-      res.status(200)
-        .json({
-          success: true,
-        });
-    })
+  const user = await User.findOne({username: u.username})
     .catch(err => {
+      // console.log(err);
       next(err);
+    });
+  if (!user) {
+    res.status(401)
+      .json({success: false, msg: 'could not find user'});
+    return;
+  }
+  res.status(200)
+    .json({
+      success: true,
     });
 });
 
